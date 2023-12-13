8ab13abad90afaa4e85f5e52b84896aa48b4a992
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-13 19:39:50 +0100

    Command-line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main(){
+	//Defines a new command-line flag.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//This parses the command-line flag, which in turn makes it possible to read in the command-line flag value and assigns it to the addr variable.
+	//Ex: go run . -addr=":9999"
+	flag.Parse()
 	//Golang has a http.DefaultServeMux BUT for the sake of clarity, maintainablility and security, it's generally a good idea to create your own.
 	mux := http.NewServeMux()
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
@@ -13,9 +19,9 @@ func main(){
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetViewHandler)
 	mux.HandleFunc("/snippet/create", snippetCreateHandler)
-	log.Println("Starting server on: 3000")
+	log.Printf("Starting server on %s", *addr)
 
-	err := http.ListenAndServe(":3000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 	
 }
