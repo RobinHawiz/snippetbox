90ddaac39d87fc040934c62ebf4ac6118397a57f
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-13 19:14:19 +0100

    Access files in website

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,8 @@ import (
 func main(){
 	//Golang has a http.DefaultServeMux BUT for the sake of clarity, maintainablility and security, it's generally a good idea to create your own.
 	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetViewHandler)
 	mux.HandleFunc("/snippet/create", snippetCreateHandler)
