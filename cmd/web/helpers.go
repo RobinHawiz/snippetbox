1c01fcbdab8faa06c693930e2f5711488162935a
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-14 08:07:04 +0100

    Added stack trace in serveError

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"runtime/debug"
 )
 
 func (a *application) serverError(w http.ResponseWriter, r *http.Request, err error){
-	a.logger.Error(err.Error(), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
+	a.logger.Error(err.Error(), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("trace", string(debug.Stack())))
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
