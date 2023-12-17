9647befc08b18f1c2e9adc96b1b5c4448def6368
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-17 22:00:40 +0100

    Clean URLs and method-based routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,30 +6,32 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/robinhawiz/snippetbox/internal/models"
 )
 
 func (a *application) home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/"{
-		a.notFound(w)
-		return
-	}else{
-		snippets, err := a.snippets.Latest()
-		if err != nil {
-			a.serverError(w,r,err)
-		}
-		data := a.newTemplateData(r)
-		data.Snippets = snippets
-		a.render(w,r,http.StatusOK,"home.tmpl",data)
+	snippets, err := a.snippets.Latest()
+	if err != nil {
+		a.serverError(w,r,err)
 	}
+	data := a.newTemplateData(r)
+	data.Snippets = snippets
+	a.render(w,r,http.StatusOK,"home.tmpl",data)
 }
 
 func (a *application) snippetViewHandler(w http.ResponseWriter, r *http.Request){
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	//httprouter when parsing a request puts any named parameters in the request context.
+	//in this instance params becomes a slice with a key (id) and a corresponding value.
+	params := httprouter.ParamsFromContext(r.Context())
+
+	//We use the ByName() method to get the value of the "id" named parameter from the slice and valitdate it.
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		a.notFound(w)
 		return
 	}
+
 	snippet, err := a.snippets.Get(id)
 	if err != nil{
 		if errors.Is(err, models.ErrNoRecord){
@@ -39,22 +41,24 @@ func (a *application) snippetViewHandler(w http.ResponseWriter, r *http.Request)
 		}
 			return
 		}
+
 	data := a.newTemplateData(r)
 	data.Snippet = snippet
+
 	a.render(w,r,http.StatusOK,"view.tmpl",data)
 }
 
 func (a *application) snippetCreateHandler(w http.ResponseWriter, r *http.Request){
-	if(r.Method != http.MethodPost){
-		w.Header().Set("Allow", http.MethodPost)
-		a.clientError(w,http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+func (a *application) snippetCreatePostHandler(w http.ResponseWriter, r *http.Request){
+
 	id, err := a.snippets.Insert("Snails: 101", "Snails move surprisingly slowly. But while snails may not be the fastest creatures, their steady pace shows that sometimes perseverance is more important than speed.", 1)
 	if err != nil {
 		a.serverError(w,r,err)
 		return
 	}
 
-	http.Redirect(w,r,fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w,r,fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,20 +3,30 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (a *application) routes() http.Handler{
-		//Golang has a http.DefaultServeMux BUT for the sake of clarity, maintainablility and security, it's generally a good idea to create your own.
-		mux := http.NewServeMux()
+		//Initialize router.
+		router := httprouter.New()
+		//We set our own helper function to be called when the router needs to send a 404 response. Router will otherwise by default use http.NotFound().
+		router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			a.notFound(w)
+		})
+
+		//Load the static files into the website
 		fileserver := http.FileServer(http.Dir("./ui/static/"))
-		mux.Handle("/static/", http.StripPrefix("/static", fileserver))
-		mux.HandleFunc("/", a.home)
-		mux.HandleFunc("/snippet/view", a.snippetViewHandler)
-		mux.HandleFunc("/snippet/create", a.snippetCreateHandler)
+		router.Handler("GET", "/static/*filepath", http.StripPrefix("/static", fileserver))
+
+		//Routing
+		router.HandlerFunc("GET", "/", a.home)
+		router.HandlerFunc("GET", "/snippet/view/:id", a.snippetViewHandler)
+		router.HandlerFunc("GET", "/snippet/create", a.snippetCreateHandler)
+		router.HandlerFunc("POST", "/snippet/create", a.snippetCreatePostHandler)
 
 		//Creating a middleware chain containing our "standard" middleware which will be used for every request our application recieves.
 		standard := alice.New(a.recoverPanic, a.logRequest, secureHeaders)
-		return standard.Then(mux)
+		return standard.Then(router)
 }
 	
